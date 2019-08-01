224146dcc5c9b1b89bb31370305ab805463f7d50
Erik Aas <esraiak@gmail.com>
2019-08-01 14:31:11 +0200

    add unbiased binary search

---
+++ allt.h
@@ -0,0 +1,11 @@
+#ifndef ALLT_H
+#define ALLT_H
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <cmath>
+#define rep(i,a,b) for(int i=(a); i<(b);++i)
+#define trav(it, v) for(typeof((v).begin()) it = (v).begin(); it != (v).end(); ++it)
+using namespace std;
+
+#endif
